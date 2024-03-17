205c09ca317913264743bcf6ba6e249b91b74e57
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-17 10:04:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// Q no 25
+var AlienColor = "Red";
+if (AlienColor === "Red") {
+    console.log("you earn 5 points");
+}
+var Alien_Color = "Red";
+if (AlienColor === "Yellow") {
+    console.log("you earn 5 points");
+}
+// Q no 26
+var Alien_Color0 = "Green";
+if (Alien_Color0 === "Green") {
+    console.log("You earn 5 points");
+}
+else {
+    console.log("You lost 10 points ");
+}
+if (Alien_Color0 === "Yellow") {
+    console.log("You earn 5 points ");
+}
+else {
+    console.log("You lost 10 points ");
+}
---
+++ main.ts
@@ -0,0 +1,68 @@
+// Q no 25
+let AlienColor :string ="Red";
+if(AlienColor === "Red"){
+    console.log("you earn 5 points");
+}
+
+let Alien_Color :string ="Red";
+if(AlienColor === "Yellow"){
+    console.log("you earn 5 points");
+}
+
+// Q no 26
+let Alien_Color0 :string ="Green"
+if(Alien_Color0 === "Green"){
+    console.log("You earn 5 points");
+}
+else{
+    console.log("You lost 10 points ");
+}
+
+if(Alien_Color0 === "Yellow"){
+    console.log("You earn 5 points ");
+}
+else{
+    console.log("You lost 10 points ");
+}
+
+//  Q no 27
+// Green Alien version
+let Alien_Color1 :string ="Green"
+if(Alien_Color1 === "Green"){
+    console.log("You earn 5 dollars"); 
+}
+else if(Alien_Color1 === "Yellow"){
+    console.log("You earn 10 dollars");
+}
+else if(Alien_Color1 === "Red"){
+    console.log("You earn 15 dollars");
+}
+
+// Yellow Alien version
+let Alien_Color2 :string ="Yellow"
+if(Alien_Color2 === "Green"){
+    console.log("You earn 5 dollars"); 
+}
+else if(Alien_Color2 === "Yellow"){
+    console.log("You earn 10 dollars");
+}
+else if(Alien_Color2 === "Red"){
+    console.log("You earn 15 dollars");
+}
+
+// Red Alien version
+let Alien_Color3 :string ="Red"
+if(Alien_Color3 === "Green"){
+    console.log("You earn 5 dollars"); 
+}
+else if(Alien_Color3 === "Yellow"){
+    console.log("You earn 10 dollars");
+}
+else if(Alien_Color3 === "Red"){
+    console.log("You earn 15 dollars");
+}
+
+
+
+
+
